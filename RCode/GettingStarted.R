60a0e3d73397e3eccb35c34382209c74dfd35289
Mike Treglia <mtreglia@gmail.com>
2018-01-07 15:03:35 -0500

    adding everything to folder

---
+++ RCode/GettingStarted.R
@@ -0,0 +1,18 @@
+#Useful links: https://tclavelle.github.io/blog/blogdown_github/; https://bookdown.org/yihui/blogdown/a-quick-example.html 
+#Check out the blackburn theme https://themes.gohugo.io/blackburn/
+
+install.packages("blogdown")
+blogdown::install_hugo()
+install.packages("later")
+
+
+blogdown::new_site()
+setwd("~/Dropbox/Blogdown_Exploration1/")
+getwd()
+#blogdown::new_site(dir = "Blogdown_site_files")
+getwd()
+setwd("Blogdown_site_files/")
+
+blogdown::build_site()
+file.edit(".Rprofile")
+blogdown::serve_site()
